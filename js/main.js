3b9248d4a5057179f7096fb88f359b6916edcdae
igorigor202 <55034950+igorigor202@users.noreply.github.com>
2022-07-25 20:37:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', function() {
+    menuList.classList.toggle('menu__list--active');
+});
